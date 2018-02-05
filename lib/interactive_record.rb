0f985766385af6bcb19fecf7d8b24a1e9dc5467d
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-02-05 18:55:16 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -15,50 +15,50 @@ def self.column_names
 
   table_info.each do |column|
     column_names << column["name"]
-  end 
+  end
   column_names
-end 
+end
 
 def initialize(options={})
   options.each do |property, value|
     self.send("#{property}=", value)
-  end 
+  end
 end
 
 def table_name_for_insert
   self.class.table_name
-end 
+end
 
 def col_names_for_insert
   self.class.column_names.delete_if{|c|c == "id"}.join(", ")
-end 
+end
 
 def values_for_insert
   values = []
   self.class.column_names.each do |col_name|
     values << "'#{send(col_name)}'" unless send(col_name).nil?
-  end 
+  end
   values.join(", ")
-end 
+end
 
-def save 
+def save
   sql = "INSERT INTO #{self.table_name_for_insert} (#{self.col_names_for_insert}) VALUES (#{self.values_for_insert})"
   DB[:conn].execute(sql)
   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{self.table_name_for_insert}")[0][0]
-end 
+end
 
 def self.find_by_name(name)
   sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
 
   DB[:conn].execute(sql, name)
-end 
+end
 
 def self.find_by(attr)
-  key = attr.keys.first.to_s 
+  key = attr.keys.first.to_s
   value = attr.values.first
   sql = "SELECT * FROM #{self.table_name} WHERE #{key} = '#{value}'"
 
   DB[:conn].execute(sql)
-end 
+end
 
 end 
